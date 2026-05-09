4a189bc8fff7c9b55ca0455fd831022187a08b62
rishitaagarwal82-oss <rishitaagarwal82@gmail.com>
2026-05-09 16:57:39 -0700

    yuhh:

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 import pandas as pd
-df= pd.read_csv("questions.csv")
+df= pd.read_csv("questions.csv", on_bad_lines="skip")
 buttons = ["AP Calculus AB", "AP Calculus BC", "AP Statistics", "AP Computer Science A", "AP Computer Science Principles", "AP Biology", "AP Chemistry", "AP Physics 1", "AP Physics 2", "AP Physics C: Mechanics", "AP Environmental Science", "AP U.S. History", "AP World History: Modern", "AP European History", "AP Psychology", "AP U.S. Government and Politics", "AP English Language and Composition", "AP English Literature and Composition", "AP Spanish Language and Culture", "AP French Language and Culture"]
 
 # DEFINE VARIABLES
