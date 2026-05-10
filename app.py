901796e270bf9ec08b31b5b1958d54dc6302dd64
rishitaagarwal82-oss <rishitaagarwal82@gmail.com>
2026-05-09 17:49:59 -0700

    yuh

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ df= pd.read_csv("questions.csv", on_bad_lines="skip")
 buttons = ["AP Calculus AB", "AP Calculus BC", "AP Statistics", "AP Computer Science A", "AP Computer Science Principles", "AP Biology", "AP Chemistry", "AP Physics 1", "AP Physics 2", "AP Physics C: Mechanics", "AP Environmental Science", "AP U.S. History", "AP World History: Modern", "AP European History", "AP Psychology", "AP U.S. Government and Politics", "AP English Language and Composition", "AP English Literature and Composition", "AP Spanish Language and Culture", "AP French Language and Culture"]
 
 # DEFINE VARIABLES
-if "answered_index" not in st.session_state:
-    st.session_state.answered_index = set()
+
 if "correct_items" not in st.session_state:
     st.session_state.correct_items = 0
 if "questions_answered" not in st.session_state:
@@ -85,10 +84,6 @@ if st.session_state.page == "quiz":
         st.session_state.questions_answered = 0
         st.rerun()
     
-
-
-    if "q_index" not in st.session_state:
-        st.session_state.q_index = 0
     if "last_q" not in st.session_state:
         st.session_state.last_q = None
     if st.session_state.q_index != st.session_state.last_q:
@@ -126,14 +121,6 @@ if st.session_state.page == "quiz":
                 st.session_state.correct_items+=1
         st.session_state.iscorrect = is_correct
 
-        if st.session_state.q_index not in st.session_state.answered_index:
-            st.session_state.questions_answered +=1
-            st.session_state.answered_index.add(st.session_state.q_index)
-        st.rerun()
-       
-        
-        
- 
  # RESULTS
     if st.session_state.submitted:
         if st.session_state.iscorrect == True:
@@ -152,7 +139,6 @@ if st.session_state.page == "quiz":
             if st.button("Try again"):
                 st.session_state.submitted = False
                 st.session_state.iscorrect = None
-                st.session_state.q_index = st.session_state.q_index
                 st.rerun()
         st.write("Correct questions:", st.session_state.correct_items)
         st.write("Questions Answered:", st.session_state.questions_answered)
