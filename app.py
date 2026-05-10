a4a40271a5e97641af3e49860a42d91f2b6e33f6
rishitaagarwal82-oss <rishitaagarwal82@gmail.com>
2026-05-09 18:12:51 -0700

    yuh

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ df= pd.read_csv("questions.csv", on_bad_lines="skip")
 buttons = ["AP Calculus AB", "AP Calculus BC", "AP Statistics", "AP Computer Science A", "AP Computer Science Principles", "AP Biology", "AP Chemistry", "AP Physics 1", "AP Physics 2", "AP Physics C: Mechanics", "AP Environmental Science", "AP U.S. History", "AP World History: Modern", "AP European History", "AP Psychology", "AP U.S. Government and Politics", "AP English Language and Composition", "AP English Literature and Composition", "AP Spanish Language and Culture", "AP French Language and Culture"]
 
 # DEFINE VARIABLES
-if "answered_index" not in st.session_state:
-    st.session_state.answered_index= set()
+
 if "correct_items" not in st.session_state:
     st.session_state.correct_items = 0
 if "questions_answered" not in st.session_state:
@@ -33,8 +32,7 @@ if "page" not in st.session_state:
     st.session_state.page = "home"
 if "last_ap" not in st.session_state:
     st.session_state.last_ap = None
-if "correct_index" not in st.session_state:
-    st.session_state.correct_index = set()
+
 if "first_try" not in st.session_state:
     st.session_state.first_try = {}
 if "q_index" not in st.session_state:
@@ -69,9 +67,12 @@ if st.session_state.page == "quiz":
             score = 0
 
         st.success(f"🎉 You finished! Your score is {score * 100:.1f}%!")
-        st.session_state.q_index = 0
-        st.session_state.correct_items= 0
-        st.session_state.questions_answered = 0
+        if st.button("Return Home"):
+            st.session_state.q_index = 0
+            st.session_state.correct_items= 0
+            st.session_state.questions_answered = 0
+            st.session_state.page = "home"
+            st.rerun()
         st.stop()
 
     def current_question():
@@ -81,8 +82,9 @@ if st.session_state.page == "quiz":
         st.session_state.q_index = 0
         st.session_state.submitted = False
         st.session_state.iscorrect = None
-        st.session_state.correct_items= 0
+        st.session_state.correct_items = 0
         st.session_state.questions_answered = 0
+        st.session_state.first_try = {}
         st.rerun()
     
     if "last_q" not in st.session_state:
@@ -121,17 +123,16 @@ if st.session_state.page == "quiz":
             if is_correct:
                 st.session_state.correct_items+=1
         st.session_state.iscorrect = is_correct
-        if st.session_state.q_index not in st.session_state.answered_index: 
-            st.session_state.questions_answered +=1 
-            st.session_state.answered_index.add(st.session_state.q_index)
-            st.rerun()
+        st.session_state.questions_answered += 1
+         
+            
+        st.rerun()
  # RESULTS
     if st.session_state.submitted:
         if st.session_state.iscorrect == True:
             st.success("Correct✅")        
             st.write("Explanation:", current_question()["explanation"])
-            if st.session_state.q_index not in st.session_state.correct_index:
-                st.session_state.correct_index.add(st.session_state.q_index)
+           
             if st.button("Next"):
                 st.session_state.q_index +=1
                 st.session_state.iscorrect = None
@@ -151,10 +152,7 @@ if st.session_state.page == "quiz":
 
  # RESET AND CHANGE QUIZZES
     
-    if st.session_state.last_ap != st.session_state.ap:
-        st.session_state.submitted = False
-        st.session_state.iscorrect = None
-        st.session_state.last_ap = st.session_state.ap
+
 
 
    
